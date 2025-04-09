b0735b0a35eecc49c3e321012fd7b84fbe9e8e27
ingrid Aquino <ingridaquinosoares@live.com>
2025-04-09 10:46:52 -0300

    ajustado router

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../components/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../components/LoginView.vue') // ou import direto se preferir
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/home',
-      name: 'home',
-      redirect: '/home/feeds',
-      children: [
-        {
-          path: 'feeds',
-          name: 'Feeds',
-          component: () => import('../components/HomeView.vue')
-        }
-      ]
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: () => import('../components/LoginView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes
 })
 
 export default router
