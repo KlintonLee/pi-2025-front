f91b3bd3a92c0a6bdd9aefe61c6c68e95a8579a0
KlintonLee <57025080+KlintonLee@users.noreply.github.com>
2025-04-08 23:52:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    allowedHosts: ['pi-2025-front.onrender.com'],
-  },
   plugins: [vue()],
-  base: '/',
-  server: {
-    host: '0.0.0.0',
-    port: 10000
-  }
+  base: '/'
 })
