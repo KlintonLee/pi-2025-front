3adaadea3e8d593763545dc57901cdc862ebc041
KlintonLee <57025080+KlintonLee@users.noreply.github.com>
2025-04-08 23:12:25 -0300

    chore: add host and port in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/'
+  base: '/',
+  server: {
+    host: '0.0.0.0',
+    port: 10000
+  }
 })
