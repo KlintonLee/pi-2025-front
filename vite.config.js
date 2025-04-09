4d3a56f7fca0f7ce0b244142baf6b9001024fffa
KlintonLee <57025080+KlintonLee@users.noreply.github.com>
2025-04-08 23:26:55 -0300

    chore: allow onrender in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['pi-2025-front.onrender.com'],
+  },
   plugins: [vue()],
   base: '/',
   server: {
